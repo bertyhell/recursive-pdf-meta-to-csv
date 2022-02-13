f7ca0f1b8f21a1e5418475c04922e4be51d2542f
Bert Verhelst <verhelstbert@gmail.com>
2022-02-13 19:37:56 +0100

    feat(initial): initial working version

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # recursive-pdf-meta-to-csv
-Get meta data from all pdf files in a folder and its subfolders and output it to csv format (windows support only)
+Get metadata from all pdf files in a folder and its subfolders and output it to csv format (windows support only)
+
+
+![](console.png)
+
+![](output.png)
---
+++ src/index.ts
@@ -0,0 +1,58 @@
+import * as fs from 'fs/promises';
+import * as path from 'path';
+import { promisify } from 'util';
+import glob from "glob-promise";
+
+async function getPdfInfo(file: string): Promise<Record<string, string>> {
+	const exec = promisify(require('child_process').exec);
+	const output = await exec(`node_modules\\.bin\\pdf-meta-editor.cmd "${file}" -p`);
+	const outputString = output.stdout.trim();
+	const meta: Record<string, string> = {};
+	outputString.split('\n').slice(1).map((line: string) => line.split(':')).forEach((keyValue: [string, string]) => {
+		if (keyValue[1] && !keyValue[1].includes('undefined')) {
+			meta[keyValue[0]] = keyValue[1];
+		}
+	});
+	return meta;
+}
+
+async function getPdfInfos() {
+	try {
+		const prompt = require("prompt-sync")();
+		const pdfsDir = prompt("Enter pdf folder path. eg: C:\\Users\\Me\\Documents\\my-pdfs  : ");
+
+		const files = await glob(path.join(pdfsDir, '**/*.pdf'));
+
+		let csvString = 'FilePath;FileType;FileSize;FileModifyDate;FileAccessDate;FilePermissions;PDFVersion;PageCount;Linearized;CreateDate;Creator;Producer\n';
+		for (let i = 0; i < files.length; i++) {
+
+			const filePath = files[i];
+			const info = await getPdfInfo(filePath);
+			csvString += '"' + filePath + '";"' +
+				info.FileType + '";"' +
+				info.FileSize + '";"' +
+				info.FileModifyDate + '";"' +
+				info.FileAccessDate + '";"' +
+				info.FilePermissions + '";"' +
+				info.PDFVersion + '";"' +
+				info.PageCount + '";"' +
+				info.Linearized + '";"' +
+				info.CreateDate + '";"' +
+				info.Creator + '";"' +
+				info.Producer + '"\n';
+		}
+
+		const dateFileName =new Date().toISOString().replace(/:/g, '-').replace('T', '_').replace('Z', '').split('.')[0];
+		const csvPath = path.join(pdfsDir, 'pdf-meta-data_' + dateFileName + '.csv');
+		await fs.writeFile(csvPath, csvString);
+		console.log(`Done. Found ${files.length} files.`);
+		console.log(`Wrote metadata to ` + csvPath);
+
+		prompt("\n\nPress enter to close.");
+	} catch (err) {
+		console.error('error: ', JSON.stringify(err));
+		prompt("\n\nPress enter to close.");
+	}
+}
+
+getPdfInfos();
